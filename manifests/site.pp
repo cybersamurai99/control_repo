1f062c2fce874b1c2202fc520dda0ba7310a8b55
cybersamurai99 <78857817+cybersamurai99@users.noreply.github.com>
2021-03-01 15:32:48 +0400

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -8,6 +8,16 @@ node 'jumprepo' {
  }
 
 
+  
+node 'server1' {
+ include role::master_server
+  file {'/root/README':,
+  ensure => file,
+  content => "wellcome to ${fqdn}\n",
+  owner => 'root',
+  }
+ }
+
 #node /^web/ {
 #include role::app_server
 #}
